6d600e8648300bac9607a9d55e10ecacfa541c4f
Ikeda <hayato0310@hotmail.co.jp>
2017-11-09 10:31:26 +0900

    エラー　修正

--- Cosmic_Life/Assets/Ikeda/Script/SoundEnemy/SoundEnemy.cs
+++ Cosmic_Life/Assets/Ikeda/Script/SoundEnemy/SoundEnemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class SoundEnemy : MonoBehaviour, IEnemyEvent {
+public class SoundEnemy : Enemy {
 
     public NavMeshAgent m_SoundAgent;
 
