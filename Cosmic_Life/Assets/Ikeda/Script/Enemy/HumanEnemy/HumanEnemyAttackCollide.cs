4c9c61c2ca2eb268e6b07e82924818a4be3ee682
Ikeda <hayato0310@hotmail.co.jp>
2017-11-21 12:03:10 +0900

    HumanEnemyの攻撃が消えるようにした

---
+++ Cosmic_Life/Assets/Ikeda/Script/Enemy/HumanEnemy/HumanEnemyAttackCollide.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HumanEnemyAttackCollide : MonoBehaviour
+{
+
+    [SerializeField]
+    private int m_Damage;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        if (collision.gameObject.tag != "Player") return;
+        if (!ExecuteEvents.CanHandleEvent<IGeneralEvent>(collision.gameObject)) return;
+        //実行
+        ExecuteEvents.Execute<IGeneralEvent>(collision.gameObject, null, (e, d) => { e.onDamage(m_Damage); });
+    }
+}
