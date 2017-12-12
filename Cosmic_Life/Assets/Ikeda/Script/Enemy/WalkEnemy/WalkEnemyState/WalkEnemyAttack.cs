7ab53273d42d5a6d5d3cc13d9b1ed89af379215e
Ikeda <hayato0310@hotmail.co.jp>
2017-12-12 19:57:36 +0900

    WalkEnemy 攻撃 修正

--- Cosmic_Life/Assets/Ikeda/Script/Enemy/WalkEnemy/WalkEnemyState/WalkEnemyAttack.cs
+++ Cosmic_Life/Assets/Ikeda/Script/Enemy/WalkEnemy/WalkEnemyState/WalkEnemyAttack.cs
@@ -13,8 +13,13 @@ public class WalkEnemyAttack : EnemyState
         None
     }
 
-    //攻撃の範囲を設定
+    //攻撃の距離を設定
     [SerializeField]
+    private float m_PlayerStopDistance;
+    [SerializeField]
+    private float m_RobotStopDistance;
+
+
     private float m_DistanceCompare;
 
     //衝突判定オブジェクト
@@ -33,7 +38,9 @@ public class WalkEnemyAttack : EnemyState
 
     private float m_CoolTime;
 
-    private Vector3 m_TargetPosition;
+    private GameObject m_TargetObject;
+
+    WalkEnemy m_WalkEnemy;
 
     // Use this for initialization
     void Start()
@@ -43,11 +50,13 @@ public class WalkEnemyAttack : EnemyState
 
     //void Update()
     //{
-
     //}
 
     public override void Action(float deltaTime, Enemy enemy)
     {
+        //if (m_TargetObject == null)
+        //    m_WalkEnemy.ChangeState(EnemyStatus.ReturnPosition);
+
         switch (m_AttackState)
         {
             case AttackState.Attack:
@@ -64,28 +73,35 @@ public class WalkEnemyAttack : EnemyState
 
             case AttackState.AttackAfter:
                 m_AttackCollider.SetActive(false);
-                WalkEnemy l_WalkEnemy = enemy.GetComponent<WalkEnemy>();
+                if (m_WalkEnemy == null)
+                    m_WalkEnemy = enemy.GetComponent<WalkEnemy>();
+                //WalkEnemy l_WalkEnemy = enemy.GetComponent<WalkEnemy>();
                 //NULLだったら状態を変更
-                if (m_TargetPosition == null)
-                    m_TargetPosition = l_WalkEnemy.CheckPlayerAndRobot().transform.position;
-                else enemy.ChangeState(EnemyStatus.ChasingButLosed);
-
+                m_TargetObject = m_WalkEnemy.CheckPlayerAndRobot();
+                if (m_TargetObject == null)
+                {
+                    m_WalkEnemy.m_Agent.isStopped = false;
+                    m_WalkEnemy.ChangeState(EnemyStatus.ReturnPosition);
+                    return;
+                }
 
                 //クールタイムを調べる
                 if (m_CoolTime >= 0) m_CoolTime -= deltaTime;
 
+                CheckStopDistance(enemy);
+
                 //攻撃後のプレイヤーとの距離を測って、離れていたら追跡中に変更
-                float distance = (Vector3.Distance(l_WalkEnemy.GetEnemyPosition(), m_TargetPosition));
+                float distance = (Vector3.Distance(m_WalkEnemy.GetEnemyPosition(), m_TargetObject.transform.position));
                 if (distance < m_DistanceCompare)
                 {
                     //近ければプレイヤーの方向を向いて攻撃
-                    Vector3 relativePos = l_WalkEnemy.CheckPlayerAndRobot().transform.position - l_WalkEnemy.transform.position;
+                    Vector3 relativePos = m_WalkEnemy.CheckPlayerAndRobot().transform.position - m_WalkEnemy.transform.position;
                     relativePos.y = 0;
                     Quaternion rotation = Quaternion.LookRotation(relativePos);
-                    l_WalkEnemy.transform.rotation = Quaternion.Slerp(l_WalkEnemy.transform.rotation, rotation, Time.deltaTime * 2.0f);
+                    m_WalkEnemy.transform.rotation = Quaternion.Slerp(m_WalkEnemy.transform.rotation, rotation, Time.deltaTime * 2.0f);
 
                     //自身の前方向とプレイヤーとの角度を調べる
-                    if (Vector3.Angle(l_WalkEnemy.transform.forward, relativePos) <= 1.5f && m_CoolTime <= 0)
+                    if (Vector3.Angle(m_WalkEnemy.transform.forward, relativePos) <= 1.5f && m_CoolTime <= 0)
                     {
                         m_AttackState = AttackState.Attack;
                     }
@@ -93,10 +109,25 @@ public class WalkEnemyAttack : EnemyState
                 else
                 {
                     m_AttackState = AttackState.Attack;
-                    l_WalkEnemy.m_Agent.isStopped = false;
+                    m_WalkEnemy.m_Agent.isStopped = false;
                     enemy.ChangeState(EnemyStatus.Chasing);
                 }
                 break;
+        }
+    }
+
+
+    private void CheckStopDistance(Enemy enemy)
+    {
+        if (enemy.GetComponent<WalkEnemy>().CheckPlayerAndRobot() == null) return;
+
+        if (enemy.GetComponent<WalkEnemy>().CheckPlayerAndRobot().transform.tag == "Player")
+        {
+            m_DistanceCompare = m_PlayerStopDistance;
+        }
+        else
+        {
+            m_DistanceCompare = m_RobotStopDistance;
         }
     }
 }
