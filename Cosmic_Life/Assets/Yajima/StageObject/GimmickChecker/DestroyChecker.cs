71acb3de7041300e6311832a625f00ee34f81565
TomoyukiYajima <shimmerofhotair@softbank.ne.jp>
2018-01-16 00:32:58 +0900

    ステージプレハブの追加、変更・ステージ遷移先の変更

---
+++ Cosmic_Life/Assets/Yajima/StageObject/GimmickChecker/DestroyChecker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyChecker : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // プレイヤーに衝突したら削除する
+        if (other.tag != "Player") return;
+        Destroy(gameObject);
+    }
+}
