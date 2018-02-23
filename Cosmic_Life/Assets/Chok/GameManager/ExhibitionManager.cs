976ebfc19238484036d2c57b6fc467bfd45c45a6
Jie-PC\Jie <jiex312@gmail.com>
2018-02-23 12:07:52 +0900

    展示用script追加

---
+++ Cosmic_Life/Assets/Chok/GameManager/ExhibitionManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExhibitionManager : SingletonBehaviour<ExhibitionManager> {
+
+	// Use this for initialization
+	void Start () {
+        Screen.fullScreen = true;
+        UnityEngine.Cursor.visible = false;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Start") && Input.GetButton("Select"))
+        {
+            if (SceneMgr.Instance.GetCurrentSceneType() == SceneType.Title) return;
+            SceneMgr.Instance.SceneTransition(SceneType.Title);
+        }
+	}
+}
